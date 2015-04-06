01c1c21c786b76291fc8d5da2467c2245838a26e
Rajarshi Nigam <raj@nigam.com>
2015-04-06 15:06:00 -0400

    Added match persisting for all regions

---
+++ cloud/jobs/persistMatches.js
@@ -0,0 +1,44 @@
+var _ = require('underscore');
+
+Parse.Cloud.job("persistMatches", function(request, status) {
+  Parse.Cloud.useMasterKey();
+  var Matches = Parse.Object.extend("Matches");
+
+  // variables that persist across promises
+  var RIOT_API_KEY, REGIONS;
+  var matches;
+
+  Parse.Config.get().then(function(config){
+    RIOT_API_KEY = config.get('RIOT_API_KEY');
+    REGIONS = config.get('REGIONS');
+  }).then(function(){
+    var beginDate = Date.now()/1000 - 600; // 10 minutes ago
+    beginDate -= beginDate%300; // floored to 5 minute increment
+
+    var promises = [];
+    _.each(REGIONS, function(region){
+      promises.push(
+        Parse.Cloud.httpRequest({
+          url: 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v4.1/game/ids',
+          params: {
+            api_key: RIOT_API_KEY,
+            beginDate: beginDate
+          }
+        }).then(function(response){
+          return new Matches({
+            region: region,
+            matches: response.data,
+            date: new Date(beginDate*1000)
+          });
+        })
+      );
+    });
+    return Parse.Promise.when(promises);
+  }).then(function(){
+    return Parse.Object.saveAll(_.toArray(arguments));
+  }).then(function(){
+    status.success('Added matches');
+  }, function(error){
+    status.error("Error: " + error.code + " " + error.message);
+  });
+});
--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,5 @@
 var updateChampions = require('cloud/jobs/updateChampions');
+var persistMatches = require('cloud/jobs/persistMatches');
 var sandbox = require('cloud/jobs/sandbox');
 
 var app = require('cloud/app.js');
