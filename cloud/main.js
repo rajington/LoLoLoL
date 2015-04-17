8d8361b5bbbead571c0bb16ed01ed22c3ecf2b4b
Rajarshi Nigam <raj@nigam.com>
2015-04-17 18:36:10 -0400

    Also removed sandbox from main

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,5 @@
 var processMatches = require('cloud/jobs/processMatches');
 var persistMatches = require('cloud/jobs/persistMatches');
 var resetData = require('cloud/jobs/resetData');
-var sandbox = require('cloud/jobs/sandbox');
 
 var app = require('cloud/app.js');
